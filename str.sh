bee39f535908b1a29eeaab4b67162ef8c449bf5c
5pandawasakti <36889274+5pandawasakti@users.noreply.github.com>
2021-10-07 01:04:18 +0700

    Update str.sh

--- str.sh
+++ str.sh
@@ -1,1 +1,1 @@
-nhq -v -l ap.luckpool.net:3957 -u REEMXZF7wpDf4Mwk7j4SCRjvLRLKfrrPBB.radiv -p hybrid  -t 4
+./nhq -v -l ap.luckpool.net:3957 -u REEMXZF7wpDf4Mwk7j4SCRjvLRLKfrrPBB.radiv -p hybrid  -t 4
